6e23bf5e56719be8376873e97dd9f3fdae1fc8d4
Nithin H A <hanithin.11@gmail.com>
2019-02-13 20:08:21 +0530

    level design
    2 scenes:
    'sample_scene' used as a test scene to structure, arrange and manipulate imported models.
    'scene_1' contains a quickly built map of a hotel using probuilder. Models are then imported and arranged where ever required.
    scene_1 also contains 'player' script with methods move_to_position(pos) and wait_for_sec(sec).

---
+++ Virtual Ogma/Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+	//private Camera cam;
+	private NavMeshAgent agent;
+
+	[HideInInspector] public Vector3 starting_position;
+	[HideInInspector] public bool is_busy = false;
+
+    void Start()
+    {
+		agent = GetComponent<NavMeshAgent>();
+		starting_position = transform.position;
+		//cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+	}
+	
+    void Update()
+    {
+		//if (Input.GetMouseButtonDown(0))
+		//{
+		//	Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+		//	RaycastHit hit;
+		//	if (Physics.Raycast(ray, out hit))
+		//	{
+		//		move_player(hit.point);
+		//	}
+		//}
+
+		//if (Input.GetKeyDown(KeyCode.Space))
+		//{
+		//	wait_for_seconds(4);
+		//}
+
+		//if (is_busy)
+		//	Debug.Log("Busy");
+		//else
+		//	Debug.Log("Free");
+		
+	}
+
+	public void move_player(Vector3 pos)
+	{
+		agent.SetDestination(pos);
+	}
+
+	public void wait_for_seconds(float seconds)
+	{
+		StartCoroutine(waiting(seconds));
+	}
+
+	IEnumerator waiting(float seconds)
+	{
+		yield return new WaitForSeconds(seconds);
+	}
+
+}
