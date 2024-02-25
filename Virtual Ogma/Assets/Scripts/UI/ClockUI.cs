2f64e3e55a2312cf7a27bf4bd77a12cd023d2abc
Nithin H A <NithinHA@users.noreply.github.com>
2024-02-25 05:44:28 +0530

    ClockUI cleanup.

--- Virtual Ogma/Assets/Scripts/UI/ClockUI.cs
+++ Virtual Ogma/Assets/Scripts/UI/ClockUI.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 using DG.Tweening;
 
@@ -10,7 +8,6 @@ public class ClockUI : MonoBehaviour
     [SerializeField] private RectTransform _pauseGamePos;
     [SerializeField] private RectTransform _resumeGamePos;
     [SerializeField] private TextMeshProUGUI _timeText;
-    [SerializeField] private Ease _easeMode = Ease.Linear;
 
     private GameTime _gameTime;
     private string _minutes;
@@ -33,6 +30,10 @@ public class ClockUI : MonoBehaviour
             ClockAnimationSequence();
     }
 
+    /// <summary>
+    /// Set original position and Time.
+    /// Move from _resumeGamePos to _pauseGamePos; Wait for few frames; Move from _pauseGamePos to _resumeGamePos.
+    /// </summary>
     private void ClockAnimationSequence()
     {
         if (_gameTime != null)
@@ -41,11 +42,12 @@ public class ClockUI : MonoBehaviour
         _container.SetPositionAndRotation(_resumeGamePos.position, _resumeGamePos.rotation);
 
         Sequence clockAnimSequence = DOTween.Sequence();
-        clockAnimSequence.Append(_container.DOMove(_pauseGamePos.position, 1.5f).SetEase(Ease.OutBack))
-            .Join(_container.DORotate(_pauseGamePos.rotation.eulerAngles, 1.5f).SetEase(Ease.OutBack))
+        clockAnimSequence.AppendInterval(.5f)
+            .Append(_container.DOMove(_pauseGamePos.position, 1.5f).SetEase(Ease.OutExpo))
+            .Join(_container.DORotate(_pauseGamePos.rotation.eulerAngles, 1.5f).SetEase(Ease. OutExpo))
             .AppendInterval(1.5f)
-            .Append(_container.DOMove(_resumeGamePos.position, 1.5f).SetEase(Ease.InBack))
-            .Join(_container.DORotate(_resumeGamePos.rotation.eulerAngles, 1.5f).SetEase(Ease.InBack));
+            .Append(_container.DOMove(_resumeGamePos.position, 1f).SetEase(Ease.            OutExpo))
+            .Join(_container.DORotate(_resumeGamePos.rotation.eulerAngles, 1f).SetEase(Ease.OutExpo));
     }
 
     private void OnGameTimeUpdate(float remainingTime)
